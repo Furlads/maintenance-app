22e583a9c6bb89ba57fc73ca6db02bfe6949e700
Trev Fudger <furlads@outlook.com>
2026-03-13 15:26:47 +0000

    Remove BottomNav from root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,11 @@
-import BottomNav from './components/BottomNav'
-
 export default function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode
 }) {
   return (
     <html lang="en">
-      <body
-        style={{
-          margin: 0,
-          paddingBottom: 70,
-          fontFamily: 'sans-serif'
-        }}
-      >
-        {children}
-
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
