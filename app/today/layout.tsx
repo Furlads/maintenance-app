f25c6f91c4da42ffef54389789fca895b0fa1dba
Furlads <furlads@outlook.com>
2026-08-21 20:48:39 +0100

    Enable Jacob Three Counties branding polish

--- app/today/layout.tsx
+++ app/today/layout.tsx
@@ -2,6 +2,7 @@ import type { ReactNode } from 'react'
 import prisma from '@/lib/prisma'
 import MaintenanceTodayBridge from './MaintenanceTodayBridge'
 import TodayDashboardHome from './TodayDashboardHome'
+import JacobBrandPolish from './JacobBrandPolish'
 
 export const dynamic = 'force-dynamic'
 
@@ -22,6 +23,7 @@ export default async function TodayLayout({ children }: Props) {
     <>
       <MaintenanceTodayBridge maintenanceJobIds={maintenanceJobs.map((job) => job.id)} />
       <TodayDashboardHome />
+      <JacobBrandPolish />
       {children}
     </>
   )
