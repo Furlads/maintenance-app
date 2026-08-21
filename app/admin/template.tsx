7382f40e0463006ffab4172aded01b6d513bc0b6
Furlads <furlads@outlook.com>
2026-08-21 20:34:12 +0100

    Show Trevor avatar on admin worker cards

--- app/admin/template.tsx
+++ app/admin/template.tsx
@@ -1,11 +1,13 @@
 import type { ReactNode } from 'react'
 import AdminWorkerAvatarEnhancer from './AdminWorkerAvatarEnhancer'
+import AdminTrevAvatarEnhancer from './AdminTrevAvatarEnhancer'
 
 export default function AdminTemplate({ children }: { children: ReactNode }) {
   return (
     <>
       {children}
       <AdminWorkerAvatarEnhancer />
+      <AdminTrevAvatarEnhancer />
     </>
   )
 }
