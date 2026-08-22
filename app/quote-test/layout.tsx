e8861e5bb6f0c81a3c0628af886322cf15ce2541
Furlads <furlads@outlook.com>
2026-08-22 14:23:46 +0100

    Apply stronger structured styling to CHAS quote results

--- app/quote-test/layout.tsx
+++ app/quote-test/layout.tsx
@@ -2,12 +2,14 @@ import type { ReactNode } from 'react'
 import PhotoUploadProgress from './PhotoUploadProgress'
 import PhotoUploadQueue from './PhotoUploadQueue'
 import QuoteResultPolish from './QuoteResultPolish'
+import QuoteResultReadability from './QuoteResultReadability'
 
 export default function QuoteTestLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <PhotoUploadQueue />
       <QuoteResultPolish />
+      <QuoteResultReadability />
       {children}
       <PhotoUploadProgress />
     </>
