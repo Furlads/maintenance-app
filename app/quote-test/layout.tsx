3de609baefafa540baf78e84d9bb3c9d45496509
Furlads <furlads@outlook.com>
2026-08-22 14:13:10 +0100

    Use structured CHAS quote result cards

--- app/quote-test/layout.tsx
+++ app/quote-test/layout.tsx
@@ -1,11 +1,13 @@
 import type { ReactNode } from 'react'
 import PhotoUploadProgress from './PhotoUploadProgress'
 import PhotoUploadQueue from './PhotoUploadQueue'
+import QuoteResultPolish from './QuoteResultPolish'
 
 export default function QuoteTestLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <PhotoUploadQueue />
+      <QuoteResultPolish />
       {children}
       <PhotoUploadProgress />
     </>
