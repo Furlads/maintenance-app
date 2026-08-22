5f65a977e7cf17cfbd50758df93d51024e90c3ce
Furlads <furlads@outlook.com>
2026-08-22 15:26:57 +0100

    Simplify CHAS photo client state to prevent final upload freeze

--- app/quote-test/layout.tsx
+++ app/quote-test/layout.tsx
@@ -1,25 +1,17 @@
 import type { ReactNode } from 'react'
-import PhotoUploadProgress from './PhotoUploadProgress'
-import PhotoUploadQueue from './PhotoUploadQueue'
-import PhotoStripCollapse from './PhotoStripCollapse'
 import QuoteDraftAutosave from './QuoteDraftAutosave'
 import QuoteResultPolish from './QuoteResultPolish'
 import QuoteResultReadability from './QuoteResultReadability'
 import QuoteWorkingStatus from './QuoteWorkingStatus'
-import SafeLibraryPhotoPreparation from './SafeLibraryPhotoPreparation'
 
 export default function QuoteTestLayout({ children }: { children: ReactNode }) {
   return (
     <>
-      <SafeLibraryPhotoPreparation />
-      <PhotoUploadQueue />
       <QuoteDraftAutosave />
       <QuoteResultPolish />
       <QuoteResultReadability />
       <QuoteWorkingStatus />
-      <PhotoStripCollapse />
       {children}
-      <PhotoUploadProgress />
     </>
   )
 }
