fc0960524f2954e6990e169c9132cf6d14bbb928
Furlads <furlads@outlook.com>
2026-08-22 14:26:03 +0100

    Tidy CHAS photos and show live working status

--- app/quote-test/layout.tsx
+++ app/quote-test/layout.tsx
@@ -1,8 +1,10 @@
 import type { ReactNode } from 'react'
 import PhotoUploadProgress from './PhotoUploadProgress'
 import PhotoUploadQueue from './PhotoUploadQueue'
+import PhotoStripCollapse from './PhotoStripCollapse'
 import QuoteResultPolish from './QuoteResultPolish'
 import QuoteResultReadability from './QuoteResultReadability'
+import QuoteWorkingStatus from './QuoteWorkingStatus'
 
 export default function QuoteTestLayout({ children }: { children: ReactNode }) {
   return (
@@ -10,6 +12,8 @@ export default function QuoteTestLayout({ children }: { children: ReactNode }) {
       <PhotoUploadQueue />
       <QuoteResultPolish />
       <QuoteResultReadability />
+      <QuoteWorkingStatus />
+      <PhotoStripCollapse />
       {children}
       <PhotoUploadProgress />
     </>
