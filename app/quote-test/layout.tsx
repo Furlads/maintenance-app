527c420db55f28af2faa12932b3372f01de3d1e2
Furlads <furlads@outlook.com>
2026-08-22 13:39:20 +0100

    Limit CHAS photo uploads to two at a time

--- app/quote-test/layout.tsx
+++ app/quote-test/layout.tsx
@@ -1,9 +1,11 @@
 import type { ReactNode } from 'react'
 import PhotoUploadProgress from './PhotoUploadProgress'
+import PhotoUploadQueue from './PhotoUploadQueue'
 
 export default function QuoteTestLayout({ children }: { children: ReactNode }) {
   return (
     <>
+      <PhotoUploadQueue />
       {children}
       <PhotoUploadProgress />
     </>
