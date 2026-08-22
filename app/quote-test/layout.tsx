831e04556ef79a79737fe83dc316ea752dcbb982
Furlads <furlads@outlook.com>
2026-08-22 14:29:10 +0100

    Enable reliable CHAS quote autosave

--- app/quote-test/layout.tsx
+++ app/quote-test/layout.tsx
@@ -2,6 +2,7 @@ import type { ReactNode } from 'react'
 import PhotoUploadProgress from './PhotoUploadProgress'
 import PhotoUploadQueue from './PhotoUploadQueue'
 import PhotoStripCollapse from './PhotoStripCollapse'
+import QuoteDraftAutosave from './QuoteDraftAutosave'
 import QuoteResultPolish from './QuoteResultPolish'
 import QuoteResultReadability from './QuoteResultReadability'
 import QuoteWorkingStatus from './QuoteWorkingStatus'
@@ -10,6 +11,7 @@ export default function QuoteTestLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <PhotoUploadQueue />
+      <QuoteDraftAutosave />
       <QuoteResultPolish />
       <QuoteResultReadability />
       <QuoteWorkingStatus />
