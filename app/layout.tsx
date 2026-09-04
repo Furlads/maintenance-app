5be5c08c60f008240f1275d9fb1d41fa06c371e7
Furlads <furlads@outlook.com>
2026-09-04 17:14:43 +0100

    Apply shared app visual shell across all routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className="app-root">
         <ServiceWorkerRegister />
         <ChasEnhancements />
         <ThreeCountiesWorkerTheme />
